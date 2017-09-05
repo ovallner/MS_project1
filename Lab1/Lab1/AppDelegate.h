7a34655880d550f43442dc2f008227308af4b5f5
Lucas Hansen <lchansen97@gmail.com>
2017-09-05 17:29:48 -0500

    init commit

---
+++ Lab1/Lab1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Lab1
+//
+//  Created by Luke Hansen on 9/5/17.
+//  Copyright © 2017 Luke Hansen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Lab1/Lab1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Lab1
+//
+//  Created by Luke Hansen on 9/5/17.
+//  Copyright © 2017 Luke Hansen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
