04015e3ebadde9f4b2e5d02350092af97dc6cd5b
ovallner <ovallner@gmail.com>
2017-09-06 22:17:34 -0500

    gets the first 5 pokemon

--- Lab1/Lab1/ViewController.m
+++ Lab1/Lab1/ViewController.m
@@ -9,14 +9,41 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (strong, nonatomic) NSMutableArray *pokemonData;
+
 
 @end
 
 @implementation ViewController
 
+-(NSMutableArray*)pokemonData{
+    if(!_pokemonData) {
+        _pokemonData = [NSMutableArray new];
+        NSError *e = nil;
+        NSMutableString *urlString;
+        NSURL *url;
+        NSData *data;
+        for(int i = 1; i <=5; i++){
+            NSDictionary *pokemonDict;
+            urlString = [NSMutableString stringWithFormat:@"https://pokeapi.co/api/v2/pokemon/%d/", i];
+            url = [NSURL URLWithString:urlString];
+
+            data = [NSData dataWithContentsOfURL:url];
+            pokemonDict = [NSJSONSerialization JSONObjectWithData:data options:0 error:&e];
+            [_pokemonData addObject:pokemonDict];
+            //NSLog(@"%@", _pokemonData[i-1][@"name"]);
+            
+        }
+    }
+    return _pokemonData;
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+    
+    NSLog(@"asdf %@", self.pokemonData[3][@"name"]);
+    
 }
 
 
