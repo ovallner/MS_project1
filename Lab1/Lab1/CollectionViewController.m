1c69b7e0feaacd72dff4db51a6b0692796f660da
Lucas Hansen <lchansen97@gmail.com>
2017-09-08 02:06:20 -0500

    yay

--- Lab1/Lab1/CollectionViewController.m
+++ Lab1/Lab1/CollectionViewController.m
@@ -34,15 +34,22 @@ static NSString * const reuseIdentifier = @"CollectCell";
     // Dispose of any resources that can be recreated.
 }
 
-/*
+
 #pragma mark - Navigation
 
 // In a storyboard-based application, you will often want to do a little preparation before navigation
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+    
+    BOOL isVC = [[segue destinationViewController] isKindOfClass:[TableViewController class]];
+    
+    if(isVC){
+        CollectionViewCell* cell = (CollectionViewCell*)sender;
+        TableViewController *tVc = [segue destinationViewController];
+        tVc.pokeIndex = [self.collectionView indexPathForCell:cell];
+    }
+    
 }
-*/
+
 
 #pragma mark <UICollectionViewDataSource>
 
@@ -119,11 +126,11 @@ static NSString * const reuseIdentifier = @"CollectCell";
     }
 }
 
-- (void) collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath{
-    TableViewController* tVc = [self.storyboard instantiateViewControllerWithIdentifier:@"PokePage"];
-    tVc.pokeIndex = indexPath;
-    [self.navigationController pushViewController:tVc animated:YES];
-}
+//- (void) collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath{
+//    TableViewController* tVc = [self.storyboard instantiateViewControllerWithIdentifier:@"PokePage"];
+//    tVc.pokeIndex = indexPath;
+//    [self.navigationController pushViewController:tVc animated:YES];
+//}
 
 
 @end
